b686de25fb48181bd64241f1fcfc569ea8df5262
Thiago Almeida <tbalmeida@gmail.com>
2020-12-29 10:00:41 -0700

    Add message delivery using SendGrid

---
+++ MKTFY.Api/Controllers/ContactUsController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MKTFY.Models.ViewModels;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Threading.Tasks;
+
+namespace MKTFY.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class ContactUsController : Controller
+    {
+
+        [AllowAnonymous]
+        [HttpPost]
+        public async Task<ActionResult<string>> SendMsg([FromBody] ContactUsVM src)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("Please, verify the data provided");
+            }
+            
+            await SendEmailMsg(src);
+            return Ok("Message delivered successfully");
+        }
+
+        public async Task<bool> SendEmailMsg(ContactUsVM src)
+        {
+            // SendgGrid config - using default email as sender
+            var apiKey = Environment.GetEnvironmentVariable("SendGrid");
+            var client = new SendGridClient(apiKey);
+            var from = new EmailAddress(Environment.GetEnvironmentVariable("DefaultEmail"), "MKTFY CST");
+            var to = new EmailAddress(Environment.GetEnvironmentVariable("DefaultEmail"), "MKTFY CST");
+
+            var subject = "MKTFY Contact Us - " + src.Subject;
+            var plainTextContent = $"Message from: {src.FullName} ({src.FullName})" +
+                $"\n{src.TextContent}";
+
+            var htmlContent = $"<strong>Message from</strong> {src.FullName} ({src.FullName})" +
+                $"<p>{src.TextContent}</p>";
+
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            await client.SendEmailAsync(msg);
+            return true;
+        }
+    }
+}
---
+++ MKTFY.Models/ViewModels/ContactUsVM.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace MKTFY.Models.ViewModels
+{
+    public class ContactUsVM
+    {
+        [Required]
+        [EmailAddress]
+        public string From { get; set; }
+
+        [Required]
+        public string FullName{ get; set; }
+
+        public string Subject { get; set; } = "MKTFY - Contact Us";
+
+        public string To { get; set; } = Environment.GetEnvironmentVariable("EmailTo");
+
+        [Required]
+        public string TextContent { get; set; }
+    }
+}
