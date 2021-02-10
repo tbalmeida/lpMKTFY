891349ca6477ae63d24262aaaf4691c0b0ff4034
Thiago Almeida <tbalmeida@gmail.com>
2021-02-10 15:42:33 -0700

    Added Listing/User related fields to OrderVM

--- MKTFY.Models/ViewModels/OrderVM.cs
+++ MKTFY.Models/ViewModels/OrderVM.cs
@@ -14,16 +14,33 @@ namespace MKTFY.Models.ViewModels
             BuyerId = src.BuyerId;
             Created = src.Created;
             OrderStatusId = src.OrderStatusId;
+            if (src.Listing != null)
+            {
+                Title = src.Listing.Title;
+                Price = src.Listing.Price;
+                SellerId = src.Listing.UserId;
+            }
+            Category = src.Listing.Category?.Title;
+            Seller = src.Listing.User?.FirstName + " " + src.Listing.User?.LastName;
         }
 
         public Guid Id { get; set; }
 
         public Guid ListingId { get; set; }
 
+        public string SellerId { get; set; }
+
         public string BuyerId { get; set; }
 
         public int OrderStatusId { get; set; }
 
         public DateTime Created { get; set; }
+
+        // Listing data, not always loaded
+        public string Title { get; set; }
+        public string Category { get; set; }
+        public string ItemCondition { get; set; }
+        public decimal? Price { get; set; }
+        public string Seller { get; set; }
     }
 }
